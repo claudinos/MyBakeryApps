f15dfabcafe6051fd6f77b47e916def2a8e01945
Claudine <claudinos1990@gmail.com>
2019-10-11 17:25:54 +0200

    add my main activity

---
+++ app/src/main/java/com/example/myshop/ShopActivity.java
@@ -0,0 +1,23 @@
+package com.example.myshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ShopActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+
+    }
+
+}
